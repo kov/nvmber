ec47ac57051fb1f2ea2ee5d183cca2325bd186cf
Gustavo Noronha Silva <gustavo@noronha.dev.br>
2024-07-14 11:55:26 -0300

    Massive refactoring of parsing
    We now use an enum to represent the elements and make rules clearer.
    The parser should be able to accept all valid nvmbers now, and also
    able to reject any invalid ones.
    
    This change also moves to using i64 as the main data type to represent
    nvmbers internally, making it possible to represent negative values.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 use std::{
     fmt::{Debug, Display},
-    ops::{Add, Sub},
+    ops::{Add, Neg, Sub},
     str::FromStr,
 };
 
 use thiserror::Error as ThisError;
 
-#[derive(ThisError, Debug)]
+#[derive(ThisError, Debug, PartialEq)]
 pub enum Error {
     #[error("nvmber too large")]
     NvmberTooLarge(String),
@@ -15,10 +15,217 @@ pub enum Error {
     Malformed(String),
 }
 
+#[repr(i64)]
+#[derive(Clone, Debug, PartialEq, PartialOrd)]
+enum Numeri {
+    I = 1,
+    V = 5,
+    X = 10,
+    L = 50,
+    C = 100,
+    D = 500,
+    M = 1000,
+}
+
+impl Display for Numeri {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let c = match self {
+            Numeri::I => 'I',
+            Numeri::V => 'V',
+            Numeri::X => 'X',
+            Numeri::L => 'L',
+            Numeri::C => 'C',
+            Numeri::D => 'D',
+            Numeri::M => 'M',
+        };
+        write!(f, "{c}")
+    }
+}
+
+impl Numeri {
+    fn is_beyond_max_repeat(&self, repeated: usize) -> bool {
+        // No element can go beyond 3, so 2 repeats is the max allowed.
+        if repeated > 2 {
+            return true;
+        }
+
+        match self {
+            Numeri::V | Numeri::L | Numeri::D => repeated > 0,
+            _ => false,
+        }
+    }
+
+    fn is_allowed_after(&self, other: &Numeri, other_repeat: usize) -> bool {
+        match self {
+            Numeri::I => true,
+            Numeri::V => match other {
+                // V can come after anything, but if it comes after I, that
+                // is being used to modify it and can only be there once.
+                Numeri::I => other_repeat == 0,
+                _ => true,
+            },
+            Numeri::X => match other {
+                Numeri::I => other_repeat == 0,
+                Numeri::V => false,
+                _ => true,
+            },
+            Numeri::L => match other {
+                Numeri::I | Numeri::V => false,
+                Numeri::X => other_repeat == 0,
+                _ => true,
+            },
+            Numeri::C => match other {
+                Numeri::I | Numeri::V | Numeri::L => false,
+                Numeri::X => other_repeat == 0,
+                _ => true,
+            },
+            Numeri::D => match other {
+                Numeri::I | Numeri::V | Numeri::X | Numeri::L => false,
+                Numeri::C => other_repeat == 0,
+                _ => true,
+            },
+            Numeri::M => match other {
+                Numeri::I | Numeri::V | Numeri::L | Numeri::D => false,
+                Numeri::C => other_repeat == 0,
+                _ => true,
+            },
+        }
+    }
+}
+
+struct Parser {
+    previous: Option<Numeri>,
+    previous_repeat: usize,
+    last: Option<Numeri>,
+    current_repeat: usize,
+    remaining: Vec<Numeri>, // Reversed.
+}
+
+impl Parser {
+    fn new(s: &str) -> Result<Self, Error> {
+        let numeri = s
+            .chars()
+            .rev()
+            .map(|c| {
+                let n = match c {
+                    'I' => Numeri::I,
+                    'V' => Numeri::V,
+                    'X' => Numeri::X,
+                    'L' => Numeri::L,
+                    'C' => Numeri::C,
+                    'D' => Numeri::D,
+                    'M' => Numeri::M,
+                    _ => return Err(Error::Malformed(format!("Unkwown element {c} in {s}"))),
+                };
+                Ok(n)
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(Parser {
+            previous: None,
+            previous_repeat: 0,
+            last: None,
+            current_repeat: 0,
+            remaining: numeri,
+        })
+    }
+
+    fn step(&mut self) -> Result<i64, Error> {
+        if self.remaining.is_empty() {
+            return Ok(0);
+        }
+
+        // SAFETY: the check above means we will always have an item to pop.
+        let c = self.remaining.pop().unwrap();
+
+        if let Some(previous) = &self.previous {
+            if !c.is_allowed_after(previous, self.previous_repeat) {
+                return Err(Error::Malformed(format!(
+                    "Found {c} after {previous}, which is not allowed"
+                )));
+            }
+        }
+
+        // Update repeat tracking.
+        if let Some(last) = &self.last {
+            if *last == c {
+                self.current_repeat += 1;
+            }
+        }
+
+        if c.is_beyond_max_repeat(self.current_repeat) {
+            return Err(Error::Malformed(format!(
+                "Element {c} repeated more than expected"
+            )));
+        }
+
+        if let Some(next) = self.remaining.last() {
+            // Check for elements being used to both subtract and add.
+            if let Some(previous) = &self.previous {
+                // This is only a problem if the current element is the one being modified, not the modifier. We test that by checking that the next element is larger.
+                if *previous == *next && *next < c {
+                    return Err(Error::Malformed(format!(
+                        "Found {previous} both subtracting and adding to {c}"
+                    )));
+                }
+
+                // Protect against chains of subtractions that are supported on their own.
+                // e.g. XCD, IXL.
+                if *previous < c && *next > c {
+                    return Err(Error::Malformed(format!(
+                        "Two subtractions in a row {previous} to {c} and {c} to {next}"
+                    )));
+                }
+            }
+        }
+
+        // The integer amount we need to add (or subtract, see below).
+        let mut delta = c.clone() as i64;
+
+        // Update tracking for previous if the next element is different, and reset repeat tracking.
+        if let Some(next) = self.remaining.last() {
+            if *next != c {
+                self.previous.replace(c.clone());
+                self.previous_repeat = self.current_repeat;
+                self.current_repeat = 0;
+            }
+
+            // If the next element is larger than us, that means we are being used as a subtractor.
+            if *next > c {
+                delta = -delta;
+            }
+        }
+
+        // Keep track of last element.
+        self.last.replace(c.clone());
+
+        Ok(delta)
+    }
+
+    fn parse(&mut self) -> Result<i64, Error> {
+        let mut integer = 0;
+        loop {
+            let delta = self.step()?;
+
+            if delta == 0 {
+                return Ok(integer);
+            }
+
+            integer += delta;
+
+            if integer > 3999 {
+                return Err(Error::NvmberTooLarge(format!(
+                    "Nvmber got too large (> 3,999) while parsing string"
+                )));
+            }
+        }
+    }
+}
+
 #[derive(Clone)]
 pub struct Nvmber {
     chars: String,
-    integer: u64,
+    integer: i64,
 }
 
 impl Display for Nvmber {
@@ -33,29 +240,13 @@ impl Debug for Nvmber {
     }
 }
 
-fn roman_to_integer(c: &char) -> Result<u64, Error> {
-    let int = match c {
-        '0' => 0,
-        'I' => 1,
-        'V' => 5,
-        'X' => 10,
-        'L' => 50,
-        'C' => 100,
-        'D' => 500,
-        'M' => 1000,
-        _ => return Err(Error::NvmberTooLarge(format!("{c}"))),
-    };
-
-    Ok(int)
-}
-
 impl Nvmber {
     pub fn from<S: AsRef<str>>(string: S) -> Result<Self, Error> {
         Nvmber::from_str(string.as_ref())
     }
 
     #[cfg(test)]
-    pub fn get_integer(&self) -> u64 {
+    pub fn get_integer(&self) -> i64 {
         self.integer
     }
 }
@@ -64,77 +255,7 @@ impl FromStr for Nvmber {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut integer = 0;
-        let mut prev_char: Option<char> = None;
-        let mut repeats = 0;
-        let mut prev_modifier: Option<char> = None;
-
-        s.chars().try_for_each(|c| -> Result<_, Error> {
-            let mut delta = roman_to_integer(&c)?;
-
-            if let Some(prev_char) = prev_char {
-                // If previous char is a smaller value, this means we have a modifier.
-                let prev = roman_to_integer(&prev_char)?;
-                if prev < delta {
-                    if repeats > 0 {
-                        return Err(Error::Malformed(format!(
-                            "Roman {prev_char} used as modifier to {c}, but appears more than once"
-                        )));
-                    }
-
-                    let allowed_modifiers = [
-                        ('V', 'I'),
-                        ('X', 'I'),
-                        ('L', 'X'),
-                        ('C', 'X'),
-                        ('D', 'C'),
-                        ('M', 'C'),
-                    ];
-                    if !allowed_modifiers.contains(&(c, prev_char)) {
-                        return Err(Error::Malformed(format!(
-                            "Roman {c} does not allow {prev_char} as a modifier"
-                        )));
-                    }
-
-                    prev_modifier.replace(prev_char);
-
-                    delta -= prev * 2; // * 2 because we added it on a previous loop
-                }
-
-                // Check for disallowed repeats.
-                if c == prev_char {
-                    repeats += 1;
-                } else {
-                    repeats = 0;
-                }
-
-                if repeats == 1 && ['V', 'L', 'D'].contains(&c) {
-                    return Err(Error::Malformed(format!("{} appeared twice", c)));
-                } else if repeats > 2 {
-                    return Err(Error::Malformed(format!(
-                        "{} appeared over 3 times in a row",
-                        c
-                    )));
-                }
-
-                // Check for trying to increase number that had a modifier.
-                if let Some(prev_modifier) = prev_modifier {
-                    if prev_modifier == c {
-                        return Err(Error::Malformed(format!("Trying to increase {prev_char} with {c}, but had one before as modifier")));
-                    }
-                }
-            }
-
-            integer += delta;
-
-            if integer > 3999 {
-                return Err(Error::NvmberTooLarge(format!("Nvmber got too large while parsing {}", s)));
-            }
-
-            prev_char.replace(c.clone());
-
-            Ok(())
-        })?;
+        let integer = Parser::new(s)?.parse()?;
 
         Ok(Nvmber {
             chars: s.to_owned(),
@@ -144,11 +265,13 @@ impl FromStr for Nvmber {
 }
 
 #[inline]
-fn clamp_and_convert(v: u64) -> Nvmber {
+fn clamp_and_convert(v: i64) -> Nvmber {
     let mut v = v;
 
     if v > 3999 {
         v = 3999
+    } else if v < -3999 {
+        v = -3999;
     }
 
     v.to_nvmber()
@@ -219,6 +342,25 @@ impl Sub<&Nvmber> for Nvmber {
     }
 }
 
+impl Neg for &Nvmber {
+    type Output = Nvmber;
+
+    fn neg(self) -> Self::Output {
+        Nvmber {
+            chars: self.chars.clone(),
+            integer: -self.integer,
+        }
+    }
+}
+
+impl Neg for Nvmber {
+    type Output = Nvmber;
+
+    fn neg(self) -> Self::Output {
+        (&self).neg()
+    }
+}
+
 impl Eq for Nvmber {}
 
 impl PartialEq for Nvmber {
@@ -243,21 +385,25 @@ pub trait ToNvmber {
     fn to_nvmber(&self) -> Result<Nvmber, Error>;
 }
 
-impl ToNvmber for u64 {
+impl ToNvmber for i64 {
     fn to_nvmber(&self) -> Result<Nvmber, Error> {
         let integer = *self;
 
-        if integer > 3999 {
+        if integer > 3999 || integer < -3999 {
             return Err(Error::NvmberTooLarge(format!(
-                "Nvmber can only go up to 3,999, but {} was attempted",
+                "Nvmber can only go from -3,999 to 3,999, but {} was attempted",
                 *self
             )));
         }
 
-        let intstr = integer.to_string();
         let mut chars = String::new();
 
-        integer.to_string().chars().enumerate().for_each(|(i, c)| {
+        if integer < 0 {
+            chars += "-";
+        }
+
+        let intstr = integer.abs().to_string();
+        intstr.chars().enumerate().for_each(|(i, c)| {
             let remaining = intstr.len() - i - 1;
             let (mono_char, mid_char, top_char) = match remaining {
                 0 => ('I', 'V', 'X'),
@@ -292,7 +438,7 @@ macro_rules! impl_to_nvmber {
     ($t: tt) => {
         impl ToNvmber for $t {
             fn to_nvmber(&self) -> Result<Nvmber, Error> {
-                (*self as u64).to_nvmber()
+                (*self as i64).to_nvmber()
             }
         }
     };
@@ -304,7 +450,7 @@ macro_rules! impl_to_nvmber_for_ints {
     };
 }
 
-impl_to_nvmber_for_ints![u8, u16, u32, i8, i16, i32, i64, usize];
+impl_to_nvmber_for_ints![u8, u16, u32, u64, i8, i16, i32, usize];
 
 #[cfg(test)]
 mod test {
@@ -396,10 +542,58 @@ mod test {
             3999.to_nvmber().unwrap()
         );
 
+        assert_eq!(
+            -3999.to_nvmber().unwrap() - 10.to_nvmber().unwrap(),
+            -3999.to_nvmber().unwrap()
+        );
+
         // No overflow.
         assert_eq!(
             3988.to_nvmber().unwrap() + 10.to_nvmber().unwrap(),
             3998.to_nvmber().unwrap()
         );
+    }
+
+    use super::Parser;
+
+    #[test]
+    fn test_parser() {
+        [
+            ("I", 1),
+            ("XV", 15),
+            ("CXC", 190),
+            ("CCCXC", 390),
+            ("MDC", 1600),
+        ]
+        .iter()
+        .for_each(|(s, i)| {
+            assert_eq!(
+                Parser::new(s).expect("Failed initial parse").parse(),
+                Ok(*i)
+            )
+        });
+
+        [
+            "IIII",
+            "VV",
+            "VX",
+            "IL",
+            "IIX",
+            "IXL",
+            "XIIV",
+            "XCD",
+            "XD",
+            "CDC",
+            "MMMCMXCXXII",
+        ]
+        .iter()
+        .for_each(|s| {
+            if !matches!(
+                Parser::new(s).expect("Failed initial parse").parse(),
+                Err(Error::Malformed(..))
+            ) {
+                panic!("Expected error for malformed string '{}', but it parsed", s)
+            };
+        });
     }
 }
